412fef45a15531217ea8d09dcaad92a046328866
Klara Roman <62945991+KlaraRoman@users.noreply.github.com>
2020-04-11 00:07:27 +0800

    Sources
    Sources for some of the codes I have used: including the Timeline and the animated star effects:
    
    Timeline: (pending)
    Star animation effect: https://codepen.io/saransh/pen/BKJun

---
+++ custom.js
@@ -0,0 +1,38 @@
+// Add animation/ Initialize Woo
+$(document).ready(function() {
+    
+    'use strict';
+   
+    new WOW().init();
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
